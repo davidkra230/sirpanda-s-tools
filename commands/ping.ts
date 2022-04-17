c02cbd536a90fe793c1104c7612d1832597807c8
davidkra230 <71524811+davidkra230@users.noreply.github.com>
2022-04-16 20:53:10 -0400

    features added

---
+++ commands/ping.ts
@@ -0,0 +1,9 @@
+//permissions required to use the command
+exports.permissions = ["unableiteglitgit"];
+//the description of the command
+exports.description = "get a pong with latency";
+//the run function
+exports.run = (bot:any, message:any, db:any) => {
+    //send a message to the channel that the bot is pinging with the latency
+    message.channel.send(`Pong! Latency is ${Math.round(bot.ws.ping)}ms. API Latency is ${Math.round(bot.ping)}ms`);
+}
--- index.ts
+++ index.ts
@@ -23,23 +23,45 @@ const bot = new Discord.Client({
     silent: false
 });
 
-//print something when the bot is ready and sets the status and status type to the one in the db
+//print something when the bot is ready and sets the activity and activity type to the one in the db
 bot.on(`ready`, () => {
     console.log(`Ready as: ` + bot.user.tag + `!`);
-    bot.user.setActivity(db.get(`status`));
-    bot.user.setStatus(db.get(`statusType`));
+    //set the activity and activity type to the one in the db or default to playing and "bots be like:" if there is no activity in the db
+    bot.user.setActivity(db.get("activity") || "bots be like:", { type: db.get("activityType") || "PLAYING" });
+    //also sets the prefix to "!"
+    db.set("prefix", "!");
 });
 
 //when a message is received it checks if the message is a command
-bot.on(`message`, (message: Message) => {
-    //if the message starts with the prefix specific to the one for the guild id 
-    if (message.content.startsWith(db.get(message.guild.id).prefix)) {
+bot.on(`messageCreate`, (message: Message) => {
+    //calculate prefix within a try catch block
+    try {
+        //get the prefix from the db
+        var prefix = db.get(message.guild.id).prefix;
+    } catch (error) {
+        //if there is an error set the prefix to "!"
+        db.set(`${message.guild.id}."prefix"`, db.get("prefix"));
+        //get the prefix from the db
+        var prefix = db.get("prefix");
+    }
+    var prefix = db.get(message.guild.id).prefix || db.get("prefix");
+    //if the message starts with the prefix specific to the one for the guild id or the ping of the bot 
+    if (message.content.startsWith(db.get("prefix") || `<@${bot.user.id}>`)) {
         //try to require the command file and pass everything as arguments
         try {
-            require(`./commands/` + message.content.split(` `)[0].slice(1) + `.js`)(bot, message, db);
+            //check if the user does not have the required permissions to use the command
+            if (!require(".\\commands\\" + message.content.split(" ")[0].substring(1) + ".js").permissions.includes(message.member.permissions.toArray()) || require(message.content.split(" ")[0].substring(1) + ".js").permissions !== "ALL" || !db.get("maintainers").includes(message.author.id)) {
+                //send a message to the channel that the user does not have the required permissions
+                message.channel.send(`You don't have the required permissions to use this command!`);
+                //return to stop the rest of the code from running
+                return;
+            };
+            //otherwise require the command file and pass everything as arguments
+            require(".\\commands\\" + message.content.split(" ")[0].substring(1) + ".js").run(bot, message, db);
+            
         } catch (error) {
-            //if the command file doesn't exist send an error message in the channel that the command was sent in informing the user that the command doesn't exist
-            message.channel.send(`The command doesn't exist, try ${db.get(message.guild.id).prefix}help`);
+            //if there is an error tell the user that the command is not found and tell them to use the help command
+            message.channel.send(`Command not found. Use \`${prefix}help\` to see a list of commands.\nOriginal error: ${error}`);
         }}});
 //this logs the bot in
 bot.login(process.env.TOKEN);

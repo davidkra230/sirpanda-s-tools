26662da4188ee33d20dd5c47597df0d51dd0402a
David K <davidkra230@gmail.com>
2022-06-13 12:32:21 -0400

    clarify a thing

--- commands/hiddencommandtest.ts
+++ commands/hiddencommandtest.ts
@@ -16,6 +16,6 @@ exports.data = new SlashCommandBuilder()
 exports.run = async (bot, message, db, isSlashCommand) => {
     var embed = new MessageEmbed()
     embed.setTitle("this is a hidden command")
-    embed.addFields({ name: "you can make it do anything you want", value: "you could even remove the original message to keep the command a secret." })
+    embed.addFields({ name: "you can make it do anything you want, it is hidden from the slash commands and help command", value: "you could even remove the original message to keep the command a secret." })
     message.reply({ embeds: [embed] })
 };

fa667fe774882f3dbe7a8abd25f72df9a132b740
David K <davidkra230@gmail.com>
2022-12-11 21:59:51 +0000

    e

--- webserver/server.ts
+++ webserver/server.ts
@@ -17,7 +17,7 @@ exports.server = (bot) => {
       }
     );
     app.post("/refresh", async (req, res) => {
-    console.log("repl.deploy"+req.body.toString()+req.header("Signature"));
+    console.log("repl.deploy"+JSON.stringify(req.body)+req.header("Signature"));
     })
     //start the server
     app.listen(port, () => {

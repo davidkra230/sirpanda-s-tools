3578f178c595584ba47b64cddbdb3ba9882387e9
David K <davidkra230@gmail.com>
2022-12-11 21:06:19 +0000

    repl.deploy

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
     deps = [
+        pkgs.wget
         pkgs.yarn
         pkgs.esbuild
         pkgs.nodejs-16_x
--- webserver/server.ts
+++ webserver/server.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //the server function
 exports.server = (bot) => {
     //the single route
-    app.get('/*', (req, res) => {
+    app.get('/', (req, res) => {
         //send the response
         res.send(`running as: ${bot.user.tag}<br><a href="https://discord.com/api/oauth2/authorize?client_id=727368613144821802&permissions=8&scope=bot%20applications.commands">add to server<\a>`);
     }

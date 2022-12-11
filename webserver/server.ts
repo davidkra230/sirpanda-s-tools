04cc8be1515d0d0c78fe8eb7c180f61669938ce1
David K <davidkra230@gmail.com>
2022-12-11 22:52:51 +0000

    main branch better?

--- webserver/server.ts
+++ webserver/server.ts
@@ -17,6 +17,7 @@ exports.server = (bot) => {
       }
     );
     app.post("/refresh", async (req, res) => {
+      console.log("rcved")
       console.log("repl.deploy" + JSON.stringify(req.body) + req.header("signature"));
     });
     //start the server
